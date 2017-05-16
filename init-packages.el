b503d4fbd4b4245b681ce10b5ce1612b3fb39730
Daniel Boroujerdi <daniel.boroujerdi@gmail.com>
2017-05-16 09:27:30 +0100

    fixed go-mode formatting

--- init-packages.el
+++ init-packages.el
@@ -360,14 +360,14 @@
   :init
   (progn
     (setq gofmt-command "goimports")
-    (add-hook 'before-save-hook 'gofmt-before-save))
-  (add-hook 'go-mode-hook #'global-flycheck-mode)
-  (add-hook 'go-mode-hook 'electric-pair-mode)
-  (add-hook 'go-mode-hook 'go-eldoc-setup)
+    (add-hook 'before-save-hook 'gofmt-before-save)
+    (add-hook 'go-mode-hook #'global-flycheck-mode)
+    (add-hook 'go-mode-hook 'electric-pair-mode)
+    (add-hook 'go-mode-hook 'go-eldoc-setup)
 
-  (add-hook 'go-mode-hook (lambda ()
-			    (set (make-local-variable 'company-backends) '(company-go))
-			    (company-mode)))
+    (add-hook 'go-mode-hook (lambda ()
+                              (set (make-local-variable 'company-backends) '(company-go))
+                              (company-mode))))
 
   :config
   (progn
