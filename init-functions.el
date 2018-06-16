175049434fdeec3c5f7547a63fc38c2e1d7e0460
Daniel Boroujerdi <daniel.boroujerdi@gmail.com>
2018-06-16 12:28:45 +0100

    Add json-flatten-print function

--- init-functions.el
+++ init-functions.el
@@ -31,4 +31,14 @@ point reaches the beginning or end of the buffer, stop there."
       (newline)
       (insert line-contents))))
 
+(defun json-flatten-print ()
+  (interactive)
+  (unless mark-active
+    (error "No region selected."))
+  (let ((begin (region-beginning))
+        (end (region-end)))
+    (kill-region begin end)
+    (let ((json-encoding-pretty-print nil))
+      (insert (json-encode (json-read-from-string (current-kill 0)))))))
+
 (provide 'init-functions)
